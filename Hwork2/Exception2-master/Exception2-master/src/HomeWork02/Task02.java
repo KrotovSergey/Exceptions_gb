029480ef827da6e37b8520afbf9f3246aff4b90e
KrotovSergey <serega_krotov90@mail.ru>
2023-06-15 15:56:45 +0500

    hwrk2

---
+++ Hwork2/Exception2-master/Exception2-master/src/HomeWork02/Task02.java
@@ -0,0 +1,21 @@
+// Если необходимо, исправьте данный код (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+package HomeWork02;
+public class Task02 {
+    public static void main(String[] args) {
+
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9}; /*гдето вводится массив*/
+
+        if (intArray.length > 8) {
+            try {
+                int d = 0;
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            } catch (ArithmeticException e) {
+                System.out.println("Catching exception: " + e);
+            }
+        }
+        else {
+            System.out.println("intArray contains less then 9 elements ");
+        }
+    }
+}
